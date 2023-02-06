dbcce70dd400466b1983bd504d635f26750da870
Senthil Kumar Kanagaraj <senthilkumark.nitt@gmail.com>
2023-02-06 21:30:22 +0530

    Update sql_Challenges.sql

--- Assignments/SQL/sql_Challenges.sql
+++ Assignments/SQL/sql_Challenges.sql
@@ -379,7 +379,33 @@ from prices p
 join unitsold u
 on p.product_id = u.product_id
 
+-- Q24. Write an SQL query to report the first login date for each player. Return the result table in any order
 
+-- Create Tables:
+
+create table if not exists Activity(
+	player_id int,
+	device_id int,
+	event_date date,
+	games_played int,
+	primary key (player_id, event_date)
+);
+
+
+insert into activity values
+(1, 2, '2016-03-01', 5),
+(1, 2, '2016-05-02', 6),
+(2, 3, '2017-06-25', 1),
+(3, 1, '2016-03-02', 0),
+(3, 4, '2018-07-03', 5);
+
+select * from activity;
+
+-- Solution:
+select player_id, min(event_date) first_login
+from activity
+group by player_id
+order by player_id;
 
 
 
